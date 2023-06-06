dddd44a56f8ea4713cca2afe9fedff10c224df56
fatimaaftab1388 <fatimaaftab.1388@gmail.com>
2023-06-06 12:41:59 +0900

    Fatima Aftab
    This is our java code file

---
+++ UniversityTimetableCode/src/main/java/Ui1.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class Ui1 {
+    private JButton modifyTimetableButton;
+    private JButton logoutButton;
+    private JButton viewTimetableButton;
+    private JButton generateTimetableButton;
+    private JButton button5;
+    private JButton button6;
+    private JButton button7;
+    private JComboBox comboBox1;
+    private JList list1;
+}
