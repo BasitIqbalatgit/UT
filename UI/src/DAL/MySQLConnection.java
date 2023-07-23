92ef5d118acc360855f00d50a054ac01ed338fef
BasitIqbalatgit <129366887+BasitIqbalatgit@users.noreply.github.com>
2023-07-23 04:11:17 -0700

    commit

--- UI/src/DAL/MySQLConnection.java
+++ UI/src/DAL/MySQLConnection.java
@@ -3,14 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package DAL;
-import common.utils.UserDTO;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 /**
  *
  * @author CUI
